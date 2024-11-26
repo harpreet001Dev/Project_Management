f1479690e4915323f69bb1d1a71272b35059fcbd
ProjectsKiller <harpreetkaurjs22@gmail.com>
2024-11-26 19:48:38 +0530

    added tasks  box
    added tasks  box

--- client/src/pages/UserDashboard.jsx
+++ client/src/pages/UserDashboard.jsx
@@ -1,13 +1,39 @@
 import React from 'react'
+import { IoIosClipboard } from "react-icons/io";
 
 const UserDashboard = () => {
   return (
-    <div className='flex p-5'>
-      <div className='bg-[#202020] p-8'>
-        <p className='text-white'>35</p>
-        <span className='text-red-600'>Ongoing</span>
+    <div className='w-full flex justify-between px-5 py-5'>
+    <div className='bg-[#202020] py-8 px-14 flex items-center justify-center gap-2'>
+      <IoIosClipboard color='white' size={28} />
+      <div>
+        <p className='text-white'>Total Projects</p>
+        <span className='text-red-600'>450</span>
       </div>
     </div>
+    <div className='bg-[#202020] py-8 px-14 flex items-center justify-center gap-2'>
+      <IoIosClipboard color='white' size={28} />
+      <div>
+        <p className='text-white'>Ongoing Projects</p>
+        <span className='text-red-600'>450</span>
+      </div>
+    </div>
+    <div className='bg-[#202020] py-8 px-14 flex items-center justify-center gap-2'>
+      <IoIosClipboard color='white' size={28} />
+      <div>
+        <p className='text-white'>Completed</p>
+        <span className='text-red-600'>450</span>
+      </div>
+    </div>
+    <div className='bg-[#202020] py-8 px-14 flex items-center justify-center gap-2'>
+      <IoIosClipboard color='white' size={28} />
+      <div>
+        <p className='text-white'>Total Projects</p>
+        <span className='text-red-600'>450</span>
+      </div>
+    </div>
+  </div>
+  
   )
 }
 
